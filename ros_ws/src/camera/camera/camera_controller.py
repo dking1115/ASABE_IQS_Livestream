c15f38395e9ed13ff682ecd5058f9fcef60ff597
dking1115 <davidking1115@me.com>
2024-05-20 19:19:43 -0500

    cam update

--- ros_ws/src/camera/camera/camera_controller.py
+++ ros_ws/src/camera/camera/camera_controller.py
@@ -62,7 +62,7 @@ class MyNode(Node):
         db.close()
         print(self.cams)
         for cam_key in self.cams.keys():
-            self.create_subscription(Camera,"Camera_1",partial(self.camera_msg_callback,id=cam_key),10)
+            self.create_subscription(Camera,f"Camera_{cam_key}",partial(self.camera_msg_callback,id=cam_key),10)
         # self.tf_buffer = Buffer()
         # self.tf_listener = TransformListener(self.tf_buffer, self)
         # self.tf_broadcaster = tf2_ros.TransformBroadcaster(self)
