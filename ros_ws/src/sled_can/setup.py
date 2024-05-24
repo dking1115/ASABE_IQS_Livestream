451117f67bf380dd0bc2197389f4fe11163ff342
dking1115 <davidking1115@me.com>
2024-05-24 18:32:59 -0500

    Commiting untracked files for David

---
+++ ros_ws/src/sled_can/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'sled_can'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='david',
+    maintainer_email='david@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'sled_can = sled_can.sled_can:main'
+        ],
+    },
+)
---
+++ ros_ws/src/sled_can/sled_can/sled_can.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from iqs_msgs.msg import Sled
+from sled_msgs.msg import Currentpull
+import can
+
+class MyROSNode(Node):
+    def __init__(self):
+        super().__init__('my_ros_node')
+        self.get_logger().info("My ROS 2 Node is running!")
+        self.sled_pub = self.create_publisher(Sled,"sled",10)
+        self.sled_timer=self.create_timer(.00001,self.callback_function)
+        self.bus = can.Bus(channel='can0', interface='socketcan')
+        self.current_pull_pub= self.create_publisher(Currentpull,"track_state",10)
+
+    def callback_function(self):
+        message=self.bus.recv()
+        #print(message.arbitration_id)
+        if message.arbitration_id==0x0CFF6507:
+            #print("msg")
+            self.pull_state=message.data[0]
+            self.distance=(message.data[2]|message.data[3]<<8)/12.0
+            self.force=(message.data[4]|message.data[5]<<8)/10.0
+            self.speed=((message.data[6]|message.data[7]<<8)/12.0)*0.681818
+            sled=Sled()
+            sled.header.stamp=self.get_clock().now().to_msg()
+            sled.force=self.force
+            sled.distance=self.distance
+            sled.speed=self.speed
+            self.pull_state=message.data[0]
+            if self.pull_state==1:
+                self.sled_pub.publish(sled)
+            current=Currentpull()
+            current.trackstate=self.pull_state
+            self.current_pull_pub.publish(current)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MyROSNode()
+
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+
+    # Clean up when the node is shut down
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
